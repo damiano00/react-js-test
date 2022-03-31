3d698dda98b7a904aa6124819f1a012f021350f2
Damiano Pasquini <pasquini.damiano00@gmail.com>
2022-03-31 15:39:46 +0000

    nodejs init

--- server/app.js
+++ server/app.js
@@ -1,4 +1,15 @@
 var express = require('express');
-
+const path = require('path');
 var app = express();
 
+app.get("/", function(req,res){
+   res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get("/pagina1", function(req,res){
+    res.send("<h1>Pagina1</h1>");
+});
+
+app.listen(3000,function(){
+    console.log("Server attivo sulla porta 3000");
+});
